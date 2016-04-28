995cafdd49734e7563ea13f8e956d3ae5ddbbb91
Åsmund Ødegård <mandus@gmail.com>
2016-04-28 09:17:46 +0200

    Added script that set a tex master-doc

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# scripts
+# Scripts
+
+Scripts that may or may not serve any purpose.
+
+## TeX
+
+I work quite a bit with LaTeX. When you split the sources into multiple files,
+some tools out there requires the TEX root-statement to point to the
+master-file. Since this is a trivial and boring task to maintain manually, 
+I wrote the add_tex_root.sh script to perform the task:
+
+```
+add_tex_root.sh -c <master.tex>
+```
+
+Will add "%!TEX root master.tex" as first line in all tex-files from the
+directory you are located in when running the command, and scanning recursively
+down the tree for files. Without the "-c" option, a backup of each file that
+has been changed, will be left in the folder.
+
---
+++ add_tex_root.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+#
+# (C) ExpertAnalytics 2016
+# author Åsmund Ødegård <asmund@xal.no>
+#
+# This is a script in the public domain - use however you like.
+#
+
+cleanup=no
+set -- `getopt c $*`
+for i ; do
+    case "$i"
+    in
+        -c) cleanup=yes; shift;;
+        --) shift;;
+    esac
+done
+
+if [[ $# -lt 1 ]] ; then 
+    echo "Please, specify the master tex-document as argument"
+    exit 1
+fi
+
+# set master-file, normalized
+master=$1
+[[ $master =~ ^\./.* ]] || master="./$master"
+
+if [[ ! -f $master ]] ; then 
+    echo "Unable to find master tex-document $master"
+    exit 2
+fi
+
+if [[ ${master: -4} != ".tex" ]] ; then
+    echo "Specified master $master doesn't look like a TeX source"
+    exit 3
+fi
+
+rootline="%!TEX root=$master"
+texdocs=$(find . -name '*.tex')
+
+for file in $texdocs ; do 
+    if [[ $file != $master ]] ; then 
+        echo "Edit $file"
+        firstline=$(head -1 $file)
+        # Trim leading/trailing whitespace
+        firstline="${firstline##*( )}"
+        firstline="${firstline%%*( )}"
+        #echo $firstline
+        if [[ $firstline != $rootline ]] ; then
+            if [[ $firstline =~ %.*TEX.*root.* ]] ; then
+                #echo "Need to replace rootline"
+                action="replace"
+            else
+                #echo "Need to add rootline"
+                action="addline"
+            fi
+            #echo $firstline
+        else
+            #echo "Firstline already correct"
+            action="ok"
+            #echo $firstline
+        fi
+        if [[ $action = "replace" ]] ; then 
+            tf=$(mktemp)
+            echo "$rootline" > $tf
+            tail -n +2 $file >> $tf
+            mv $file $file.back
+            mv $tf $file
+        else if [[ $action = "addline" ]] ; then
+                tf=$(mktemp)
+                echo "$rootline" > $tf
+                cat $file >> $tf
+                mv $file $file.back
+                mv $tf $file
+            fi
+        fi
+    fi
+done
+
+if [[ $cleanup = yes ]] ; then
+    find . -name '*.tex.back' -delete
+fi
